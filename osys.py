a4580ebb39c2d3e95a99a71b9dd6b285cf0fa79c
ouril <ourilkregg@gmail.com>
2017-05-31 21:08:34 +0300

    дополнина функциями удаления и копирования

--- osys.py
+++ osys.py
@@ -1,6 +1,43 @@
 import os
 import sys
-import psutil
+import psutil # модуль для управлением процессами (внешний, требует установки)
+import shutil   # модуль для манипуляией с файлами
+
+# функции для дальнейшей программы которые не обращаются к переменным модуля
+
+def help_list():
+    # словарь с инструкциями
+    help_list = {
+                'q   ': 'выход',
+                's   ': 'данные о системе',
+                'ls  ': 'содержимое текущей папки',
+                'pd  ': 'список процессов',
+                'c   ': 'копирование файла',
+                'd   ': 'улаление файла',
+                'help': 'справка'
+    }
+    # цикл для пербора ключей
+    for key in help_list.keys():
+        print("{0} - {1}".format(key, help_list[key]))
+
+def ls():
+    for i in os.listdir():
+        print(i)
+
+def mycopy():
+    ls()
+    target = str(input("Введите имя файла для копирования: "))
+    name_new = str(input("Имя нового файла: "))
+    shutil.copy(target, name_new)
+    ls()
+
+def mydel():
+    ls()
+    target = str(input("Введите имя удаляемого файла: "))
+    os.remove(target)
+    ls()
+
+# сама программа
 
 log = input("Password!\n")
 
@@ -10,30 +47,32 @@ while log:
         break
     else:
         log = input("Password!\n")
+help_list()
 
-print("[0] - Выход;")
-print("[1] - Выводить данные о системе;")
-print("[2] - Выодить список процессов;")
-print("[3] - Вывусти PID процессов.")
+doing = 'a'
 
-doing = 4
+# цикд самой программы
 while doing:
-    doing = int(input("Введите номер функции: "))
 
-    if doing == 1:
+    doing = str(input("\nВведите команду\n>> "))
+    print()
+
+    if doing == "s":
         print("Платформа ОС " , sys.platform)
         print("Кодировка файловой системы ", sys.getdefaultencoding())
         print("Имя текущего директория " , os.getcwd())
-
-    elif doing == 2:
-        print(os.listdir())
-
-    elif doing == 3:
+    elif doing == "ls":
+        ls()
+    elif doing == "pd":
         print(psutil.pids())
-
-    elif not doing:
+    elif doing == "q":
         print("EXIT!")
         break
-
+    elif doing == "c":
+        mycopy()
+    elif doing == "d":
+        mydel()
+    elif doing == "help":
+        help_list()
     else:
         print("?")
